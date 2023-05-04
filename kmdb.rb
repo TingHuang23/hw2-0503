5d8741e6fae71bbcebf23f0e317cf8fa37749492
Ting Huang <ting.huang@kellogg.northwestern.edu>
2023-05-04 06:14:34 +0000

    hw2

--- kmdb.rb
+++ kmdb.rb
@@ -85,109 +85,109 @@ new_movie["MPAA"] = "PG-13"
 new_movie["studio"] = "Warner Bros."
 new_movie.save
 
-new_movie2 = Movie.new
-new_movie2["title"] = "The Dark Knight "
-new_movie2["year_released"] = "2008"
-new_movie2["MPAA"] = "PG-13"
-new_movie2["studio"] = "Warner Bros."
-new_movie2.save
+# new_movie2 = Movie.new
+# new_movie2["title"] = "The Dark Knight "
+# new_movie2["year_released"] = "2008"
+# new_movie2["MPAA"] = "PG-13"
+# new_movie2["studio"] = "Warner Bros."
+# new_movie2.save
 
-new_movie3 = Movie.new
-new_movie3["title"] = "The Dark Knight Rises"
-new_movie3["year_released"] = "2012"
-new_movie3["MPAA"] = "PG-13"
-new_movie3["studio"] = "Warner Bros."
-new_movie3.save
+# new_movie3 = Movie.new
+# new_movie3["title"] = "The Dark Knight Rises"
+# new_movie3["year_released"] = "2012"
+# new_movie3["MPAA"] = "PG-13"
+# new_movie3["studio"] = "Warner Bros."
+# new_movie3.save
 
-new_cast = Cast.new
-new_cast["actor"] = "Christian Bale"
-new_cast["role"] = "Bruce Wayne"
-new_cast["title"] = "Batman Begins"
-new_cast.save
+# new_cast = Cast.new
+# new_cast["actor"] = "Christian Bale"
+# new_cast["role"] = "Bruce Wayne"
+# new_cast["title"] = "Batman Begins"
+# new_cast.save
 
-new_cast2 = Cast.new
-new_cast2["actor"] = "Michael Caine"
-new_cast2["role"] = "Alfred"
-new_cast2["title"] = "Batman Begins"
-new_cast2.save
+# new_cast2 = Cast.new
+# new_cast2["actor"] = "Michael Caine"
+# new_cast2["role"] = "Alfred"
+# new_cast2["title"] = "Batman Begins"
+# new_cast2.save
 
-new_cast3 = Cast.new
-new_cast3["actor"] = "Liam Neeson"
-new_cast3["role"] = "Alfred"
-new_cast3["title"] = "Ra's Al Ghul"
-new_cast3.save
+# new_cast3 = Cast.new
+# new_cast3["actor"] = "Liam Neeson"
+# new_cast3["role"] = "Alfred"
+# new_cast3["title"] = "Ra's Al Ghul"
+# new_cast3.save
 
-new_cast4 = Cast.new
-new_cast4["actor"] = "Katie Holmes "
-new_cast4["role"] = "Rachel Dawes"
-new_cast4["title"] = "Batman Begins"
-new_cast4.save
+# new_cast4 = Cast.new
+# new_cast4["actor"] = "Katie Holmes "
+# new_cast4["role"] = "Rachel Dawes"
+# new_cast4["title"] = "Batman Begins"
+# new_cast4.save
 
-new_cast5 = Cast.new
-new_cast5["actor"] = "Gary Oldman"
-new_cast5["role"] = "Commissioner Gordon"
-new_cast5["title"] = "Batman Begins"
-new_cast5.save
+# new_cast5 = Cast.new
+# new_cast5["actor"] = "Gary Oldman"
+# new_cast5["role"] = "Commissioner Gordon"
+# new_cast5["title"] = "Batman Begins"
+# new_cast5.save
 
-new_cast6 = Cast.new
-new_cast6["actor"] = "Christian Bale"
-new_cast6["role"] = "Bruce Wayne"
-new_cast6["title"] = "The Dark Knight"
-new_cast6.save
+# new_cast6 = Cast.new
+# new_cast6["actor"] = "Christian Bale"
+# new_cast6["role"] = "Bruce Wayne"
+# new_cast6["title"] = "The Dark Knight"
+# new_cast6.save
 
-new_cast7 = Cast.new
-new_cast7["actor"] = "Heath Ledger"
-new_cast7["role"] = "Joker"
-new_cast7["title"] = "The Dark Knight"
-new_cast7.save
+# new_cast7 = Cast.new
+# new_cast7["actor"] = "Heath Ledger"
+# new_cast7["role"] = "Joker"
+# new_cast7["title"] = "The Dark Knight"
+# new_cast7.save
 
-new_cast8 = Cast.new
-new_cast8["actor"] = "Aaron Eckhart"
-new_cast8["role"] = "Harvey Dent"
-new_cast8["title"] = "The Dark Knight"
-new_cast8.save
+# new_cast8 = Cast.new
+# new_cast8["actor"] = "Aaron Eckhart"
+# new_cast8["role"] = "Harvey Dent"
+# new_cast8["title"] = "The Dark Knight"
+# new_cast8.save
 
-new_cast9 = Cast.new
-new_cast9["actor"] = "Michael Caine"
-new_cast9["role"] = "Alfred"
-new_cast9["title"] = "The Dark Knight"
-new_cast9.save
+# new_cast9 = Cast.new
+# new_cast9["actor"] = "Michael Caine"
+# new_cast9["role"] = "Alfred"
+# new_cast9["title"] = "The Dark Knight"
+# new_cast9.save
 
-new_cast10 = Cast.new
-new_cast10["actor"] = "Maggie Gyllenhaal"
-new_cast10["role"] = "Rachel Dawes"
-new_cast10["title"] = "The Dark Knight"
-new_cast10.save
+# new_cast10 = Cast.new
+# new_cast10["actor"] = "Maggie Gyllenhaal"
+# new_cast10["role"] = "Rachel Dawes"
+# new_cast10["title"] = "The Dark Knight"
+# new_cast10.save
 
-new_cast11 = Cast.new
-new_cast11["actor"] = "Christian Bale"
-new_cast11["role"] = "Bruce Wayne"
-new_cast11["title"] = "The Dark Knight Rises"
-new_cast11.save
+# new_cast11 = Cast.new
+# new_cast11["actor"] = "Christian Bale"
+# new_cast11["role"] = "Bruce Wayne"
+# new_cast11["title"] = "The Dark Knight Rises"
+# new_cast11.save
 
-new_cast12 = Cast.new
-new_cast12["actor"] = "Gary Oldman"
-new_cast12["role"] = "Commissioner Gordon"
-new_cast12["title"] = "The Dark Knight Rises"
-new_cast12.save
+# new_cast12 = Cast.new
+# new_cast12["actor"] = "Gary Oldman"
+# new_cast12["role"] = "Commissioner Gordon"
+# new_cast12["title"] = "The Dark Knight Rises"
+# new_cast12.save
 
-new_cast13 = Cast.new
-new_cast13["actor"] = "Tom Hardy"
-new_cast13["role"] = "Bane"
-new_cast13["title"] = "The Dark Knight Rises"
-new_cast13.save
+# new_cast13 = Cast.new
+# new_cast13["actor"] = "Tom Hardy"
+# new_cast13["role"] = "Bane"
+# new_cast13["title"] = "The Dark Knight Rises"
+# new_cast13.save
 
-new_cast14 = Cast.new
-new_cast14["actor"] = "Joseph Gordon-Levitt"
-new_cast14["role"] = "John Blake"
-new_cast14["title"] = "The Dark Knight Rises"
-new_cast14.save
+# new_cast14 = Cast.new
+# new_cast14["actor"] = "Joseph Gordon-Levitt"
+# new_cast14["role"] = "John Blake"
+# new_cast14["title"] = "The Dark Knight Rises"
+# new_cast14.save
 
-new_cast15 = Cast.new
-new_cast15["actor"] = "Anne Hathaway"
-new_cast15["role"] = "Selina Kyle"
-new_cast15["title"] = "The Dark Knight Rises"
-new_cast15.save
+# new_cast15 = Cast.new
+# new_cast15["actor"] = "Anne Hathaway"
+# new_cast15["role"] = "Selina Kyle"
+# new_cast15["title"] = "The Dark Knight Rises"
+# new_cast15.save
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
@@ -198,8 +198,7 @@ puts "Movies"
 puts "======"
 puts ""
 
-all_movies = Movie.all
-puts all_movies.inspect
+puts Movie.inspect
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
@@ -210,8 +209,8 @@ puts "Top Cast"
 puts "========"
 puts ""
 
-all_casts = Cast.all
-puts all_casts.inspect
+# all_casts = Cast.all
+# puts all_casts.inspect
 
 # Query the cast data and loop through the results to display the cast output for each movie.
 # TODO!
